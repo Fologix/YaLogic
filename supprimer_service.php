b424f33c8542df8d75699e3492ff7ee3909dbce1
Yassine <yassine.verriez@hotmail.com>
2023-05-30 22:33:12 +0200

    services

---
+++ supprimer_service.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include_once 'db_connexion.php';
+
+if(!isset($_SESSION['user_id'])) {
+    header("Location: connexion.php");
+    exit;
+}
+
+$pdo = connexion_bdd();
+$stmt = $pdo->prepare("SELECT * FROM admin WHERE id_admin = :id_admin");
+$stmt->execute(['id_admin' => $_SESSION['user_id']]);
+$user = $stmt->fetch();
+
+if (!$user) {
+    header("Location: deconnexion.php");
+    exit;
+}
+
+if (isset($_GET['id'])) {
+    $stmt = $pdo->prepare("DELETE FROM services WHERE id_service = :id");
+    $stmt->execute(['id' => $_GET['id']]);
+}
+
+header("Location: gestion_services.php");
+exit;
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '069e31f5cd332b1d4acd917a5eb640cb5e923ca5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '069e31f5cd332b1d4acd917a5eb640cb5e923ca5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v10.14.0',
+            'version' => '10.14.0.0',
+            'reference' => 'c88a80fa7f361c2d30d6c81fb71f2bb30be1001f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
